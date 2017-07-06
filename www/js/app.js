585b730ee8a8fdead0f6b580624e1514baeb40e8
Ganesh Kothavale <ganesh.wohlig@gmail.com>
2017-07-06 21:43:30 +0530

    app.js space added 12

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'athleteController', 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+      // for form inputs) 
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         cordova.plugins.Keyboard.disableScroll(true);
